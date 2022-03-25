56ef29bd15866755db1bda4da9c807e3c5baaa0f
Rahul <Rahul@DESKTOP-LVMHKHU>
2022-03-25 03:36:00 -0700

    gh

--- JavaPrograms/src/Unit_01/P2_Task01_KeywordsInJava.java
+++ JavaPrograms/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -3,6 +3,24 @@ package Unit_01;
 public class P2_Task01_KeywordsInJava {
 public static void main(String[] args)
 {
-	System.out.println("GGF");
+	
+	//variable name
+	//Syntax error on token "double", invalid VariableDeclaratorId
+	int double = 12;
+	
+	//function name
+	int new() {
+		//sysout[ctrl+space]
+		System.out.println();
+		
+		return 0;
+	}
+	
+	
+	
+	//class name [given below]
+	
+	//object
+	//object name can never
 }
 }
